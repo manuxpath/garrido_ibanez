b136f62a88f30dac4757d1507ddde749cab08c97
Manuel Garrido <manuelgarridoserrano120@gmail.com>
2023-11-22 20:28:25 +0100

    Añadido método actualizarAlumno

--- src/AlumnoCRUD.java
+++ src/AlumnoCRUD.java
@@ -28,7 +28,19 @@ public class AlumnoCRUD {
         listaAlumno.add(alumno);
         escribirAlumnos(listaAlumno);
     }
-    
+
+    public void actualizarAlumno(Alumno alumno) {
+        ArrayList<Alumno> listaAlumnos = leerAlumnos();
+        for (int i = 0; i < listaAlumnos.size(); i++) {
+            if (listaAlumnos.get(i).getDni()
+                    .equalsIgnoreCase(alumno.getDni())) {
+                listaAlumnos.set(i, alumno);
+            }
+        }
+        escribirAlumnos(listaAlumnos);
+    }
+
+
     /*
         Escribe en el fichero un ArrayList de alumnos
      */
